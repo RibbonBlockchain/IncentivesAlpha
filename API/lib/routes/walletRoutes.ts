237d42be257f2bccbec3c7b523986175a5b7aa10
Christopher Maree <christopher.maree@gmail.com>
2019-09-16 11:50:34 +0200

    further alteration

--- API/lib/routes/walletRoutes.ts
+++ API/lib/routes/walletRoutes.ts
@@ -10,7 +10,7 @@ export class Routes {
         app.route('/')
         .get((req: Request, res: Response) => {            
             res.status(200).send({
-                message: 'GET request successfulll!!!!??<..🔥..>'
+                message: 'GET request successfulll!!!!??<...🔥...>'
             })
         })
         
