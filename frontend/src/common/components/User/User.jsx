ccf9194e5fa66b9577b40e47b2270b21772733ad
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-09-26 09:20:09 +0100

    Update logo to allow additional styling from parent components

---
+++ frontend/src/common/components/User/User.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./User.module.scss";
+
+export default function User() {
+  return (
+    <>
+      <p>User here</p>
+    </>
+  );
+}
---
+++ frontend/src/modules/Dashboard/Dashboard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Card from "../../common/components/Card";
+import styles from "./Dashboard.module.scss";
+
+export default function Dashboard() {
+  return (
+    <>
+      <h2>Title here</h2>
+      <div className={styles.dashboard}>
+        {/* <div className={styles.dashboard__item}>
+          <Card>
+          </Card>
+        </div> */}
+        {/* <div
+          className={[
+            styles.dashboard__item,
+            styles.dashboard__item__full
+          ].join(" ")}
+        >
+          <Card>
+            <img src="https://imgs.xkcd.com/comics/decline.png" alt="" />
+          </Card>
+        </div> */}
+      </div>
+    </>
+  );
+}
