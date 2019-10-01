e33a9628efc4e5e9a5eb8bcb82a7b586ce8de2a0
Christopher Maree <christopher.maree@gmail.com>
2019-10-01 17:21:27 +0200

    reverted API to use local certificates

--- API/lib/server.ts
+++ API/lib/server.ts
@@ -1,16 +1,12 @@
 import app from "./app";
-require("dotenv");
-const { SSL_KEY, SSL_CERT } = process.env;
 import * as https from "https";
 import * as fs from "fs";
 const PORT = 2053;
 
 const httpsOptions = {
-  key: String(SSL_KEY),
-  cert: String(SSL_CERT)
+  key: fs.readFileSync("./config/key.pem"),
+  cert: fs.readFileSync("./config/cert.pem")
 };
-console.log(SSL_KEY);
-console.log(SSL_CERT);
 
 https.createServer(httpsOptions, app).listen(PORT, () => {
   console.log("Express server listening on port " + PORT);
