7470217136b9aab7090db0f9eaee2f430b8b1c00
Christopher Maree <christopher.maree@gmail.com>
2019-10-17 11:39:18 +0200

    Updated contract builds for deployment

--- Blockchain/deployment/deploy.js
+++ Blockchain/deployment/deploy.js
@@ -61,7 +61,7 @@ const deploy = async (network, secret) => {
   await vaultDeployed.verboseWaitForTransaction(initVaultTx, "Init vault");
 
   //add users as admins
-  let addUser1Tx = await adminDeployed.addUser(
+  let addUser1Tx = await registryDeployed.addUser(
     "0x9A8A9958ac1B70c49ccE9693CCb0230f13F63505",
     1
   ); //issac 1
@@ -70,7 +70,7 @@ const deploy = async (network, secret) => {
     "Adding admin User 1"
   );
 
-  let addUser2Tx = await adminDeployed.addUser(
+  let addUser2Tx = await registryDeployed.addUser(
     "0xdb0B020Ab16129983045C80692fa1D1916133471",
     1
   ); //issac 2
@@ -79,7 +79,7 @@ const deploy = async (network, secret) => {
     "Adding admin User 2"
   );
 
-  let addUser3Tx = await adminDeployed.addUser(
+  let addUser3Tx = await registryDeployed.addUser(
     "0x1D4f94F90a919EB646D9158Ca3B3ECEF3EBc7941",
     1
   ); //allan
