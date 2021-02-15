b62dafbdec8d71c6633e6dc9781a9dba4779228c
Bamidele Alegbe <bamidele.alegbe@gmail.com>
2021-02-16 00:32:29 +0100

    fix

---
+++ packages/front-end/build/precache-manifest.283f7da9b11b9da03656bdb94ec444e3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad308b22b3ce6f386201b3ce08b2945",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7801712140b0374fe67",
+    "url": "/static/css/2.0c70f097.chunk.css"
+  },
+  {
+    "revision": "983546b426dac245c18f",
+    "url": "/static/css/main.1b113bac.chunk.css"
+  },
+  {
+    "revision": "d7801712140b0374fe67",
+    "url": "/static/js/2.30fd5151.chunk.js"
+  },
+  {
+    "revision": "983546b426dac245c18f",
+    "url": "/static/js/main.eda1d149.chunk.js"
+  },
+  {
+    "revision": "4061a81e7d3bcb654a62",
+    "url": "/static/js/runtime~main.f3fc4a3e.js"
+  },
+  {
+    "revision": "b59ec1ddb5b3cbf474133b866cd5482e",
+    "url": "/static/media/broken_link.b59ec1dd.png"
+  },
+  {
+    "revision": "e49a901984af05a38e23d7cb5e4395f7",
+    "url": "/static/media/loading_spinner.e49a9019.svg"
+  },
+  {
+    "revision": "9096bfb634aaaa4ac2e82ac643e100d9",
+    "url": "/static/media/login_bg.9096bfb6.svg"
+  },
+  {
+    "revision": "99088c6dbcb4cf47a811f28ad5793d7e",
+    "url": "/static/media/logo.99088c6d.svg"
+  }
+]);
